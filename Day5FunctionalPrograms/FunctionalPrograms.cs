8eea735985da3cb0d5d9c5bf49904af525af1d63
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-06-25 18:53:18 +0530

    [Muskan] Add: Flip Coin and print percentage of Heads and Tails a. I/P -> The number of times to Flip Coin. Ensure it is a positive integer. b. Logic -> Use Random Function to get value between 0 and 1. If < 0.5 then tails or heads c. O/P -> Percentage of Head vs Tails

---
+++ Day5FunctionalPrograms/FunctionalPrograms.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5FunctionalPrograms
+{
+    public class FunctionalPrograms
+    {
+        public static void FlipCoin()
+        {
+            Console.WriteLine("Enter the number of times to flip the coin: ");
+            int flips = int.Parse(Console.ReadLine());
+            int heads = 0;
+            int tails = 0;
+            Random random = new Random();
+
+            for (int i = 0; i < flips; i++)
+            {
+                double result = random.NextDouble();
+                if (result < 0.5)
+                    tails++;
+                else
+                    heads++;
+            }
+
+            double headsPercentage = (double)heads / flips * 100;
+            double tailsPercentage = (double)tails / flips * 100;
+
+            Console.WriteLine("Heads: " + headsPercentage.ToString("0.00") + "%");
+            Console.WriteLine("Tails: " + tailsPercentage.ToString("0.00") + "%");
+        }
+    }
+}
---
+++ Day5FunctionalPrograms/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5FunctionalPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select an option:");
+            Console.WriteLine("1. Flip Coin and print percentage of Heads and Tails");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                   FunctionalPrograms.FlipCoin();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    break;
+            }
+
+        }
+    }
+}
